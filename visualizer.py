0cbfae32d9a90f514c892745dff2888e2b72dcea
linyufly <linyufly@gmail.com>
2014-09-24 00:17:44 -0500

    Updated visualizer.py

--- visualizer.py
+++ visualizer.py
@@ -1,7 +1,20 @@
 import sys
+import csv
+import time
 
 def main():
   print 'File: {0}'.format(str(sys.argv[1]))
+
+  with open(sys.argv[1], 'r') as csv_file:
+    table = [row for row in csv.reader(csv_file, delimiter = ',', quotechar = '"')]
+
+  header = table[0]
+  table = [[time.strptime(row[0], '%Y/%m/%d')] +
+    [float(element) for (idx, element) in enumerate(row) if idx >= 1]
+    for (idx, row) in enumerate(table) if idx >= 2]
+
+  print header
+  print table
 
 if __name__ == '__main__':
   main()
